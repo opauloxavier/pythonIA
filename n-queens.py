49f2c837030972143e5e0079bd0ace8dad66c1c3
Thiago <thiago_d.s@hotmail.com>
2015-09-23 17:57:17 -0300

    bug novo

--- n-queens.py
+++ n-queens.py
@@ -18,17 +18,18 @@ def  rejected_spot(i,j):
 		for y in range(len(chest)):
 			if (x == i or y == j) and chest[x][y] != 5:
 				chest[x][y] = -1
-			elif  ((x == (i - line)) and (y == (j - column))) and  chest[x][y] != 5:
-				chest[x][y] = -1
+			elif  ((x == (i - line)) and (y == (j - column))):
+				if chest[x][y] != 5:
+					chest[x][y] = -1
 				line -= 1
 				column -= 1
 	print chest
 
 chest = start(sizeChest)
 
-movement(chest,2,2)
+movement(chest,4,3)
 
-rejected_spot(2,2)
+rejected_spot(4,3)
 
 #diagonais(2,2)
 #print defineDiagonal(0,0)
