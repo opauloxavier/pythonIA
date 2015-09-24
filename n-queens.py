182f9cc1c9b0d1e1338b9a24ce0d1920dbd02e98
GSegobia <gabriel_segobia@hotmail.com>
2015-09-24 01:30:58 -0300

    ta buganu menos

--- n-queens.py
+++ n-queens.py
@@ -13,12 +13,20 @@ def  rejected_spot(i,j):
 	print len(chest)
 	line = i
 	column = j
+	difference = i - j
+	diff_line = 0
+	diff_col = 0
+
+	if difference < 0:
+		diff_col = difference * (-1)
+	else:
+		diff_line = difference
 
 	for x in range(len(chest)):
 		for y in range(len(chest)):
 			if (x == i or y == j) and chest[x][y] != 5:
 				chest[x][y] = -1
-			elif  ((x == (i - line)) and (y == (j - column))):
+			elif  ((x == (i - line + diff_line)) and (y == (j - column + diff_col))):
 				if chest[x][y] != 5:
 					chest[x][y] = -1
 				line -= 1
@@ -27,9 +35,8 @@ def  rejected_spot(i,j):
 
 chest = start(sizeChest)
 
-movement(chest,4,3)
-
-rejected_spot(4,3)
+movement(chest,2,5)
+rejected_spot(2,5)
 
 #diagonais(2,2)
 #print defineDiagonal(0,0)
